09db19eb62ee34597b10f27af09f689871a0fb22
Vibhore Jain <vibhore@pcplusa.com>
2025-11-20 22:29:19 +0530

    my contradictory code will work for 74 and will not work for 84

--- sample-app/index.php
+++ sample-app/index.php
@@ -1,3 +1,14 @@
+<h1>Hi Vibhore</h1>
 <?php
+
+// This will run (with a deprecation warning) on PHP 7.4
+// but will be a fatal error in PHP 8.x+ because curly-brace
+// string offset access was removed.
+// ./lab use 74 to switch to 74
+// ./lab use 84 to switch to 84
+$str = "hello";
+$firstChar = $str{0}; // deprecated/removed syntax
+
+echo "<p>First char: $firstChar</p>";
 
 phpinfo();
